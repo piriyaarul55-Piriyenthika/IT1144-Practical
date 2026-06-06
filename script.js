c2bb2041995ce00dc37c2693c7dd92cce76c4c2c
piriyaarul55-Piriyenthika <piriyaarul55@gmail.com>
2026-06-06 20:27:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function showName(){
-	let name=document.grtElementById("name").value;
-	document.getElementById("result").innerText="Hello"+name;
-}
+
+        document.getElementById('studentForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+           
+            let name = document.getElementById('studentName').value;
+            let email = document.getElementById('studentEmail').value;
+
+            
+            alert("Success! Form submitted for: " + name);
+
+            
+            let table = document.getElementById('studentTable').getElementsByTagName('tbody')[0];
+            let newRow = table.insertRow();
+            
+            let nameCell = newRow.insertCell(0);
+            let emailCell = newRow.insertCell(1);
+            
+            nameCell.innerText = name;
+            emailCell.innerText = email;
+
+           
+            document.getElementById('studentForm').reset();
+        });
